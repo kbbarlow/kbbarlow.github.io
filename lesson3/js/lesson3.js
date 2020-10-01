ba815e2942987356b2b0cf146f4fde659ce94bb9
kbbarlow <mrsbrookebarlow@gmail.com>
2020-10-01 12:15:28 -0600

    .

---
+++ lesson3/js/lesson3.js
@@ -0,0 +1,3 @@
+const options = {year: 'numeric'};
+    document.getElementById('current-date').textContent = new Date().toLocaleDateString('en-US', options);
+document.getElementById('last-updated').innerHTML = document.lastModified;
