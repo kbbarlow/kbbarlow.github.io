25f71287689c6c745dc1512702cdff47cdcc2ed4
kbbarlow <mrsbrookebarlow@gmail.com>
2020-10-08 23:54:59 -0600

    .

--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -2,3 +2,5 @@ function toggleMenu(){
   console.log(document.getElementById("primaryNav").classList);
   document.getElementById("primaryNav").classList.toggle("hide");
 }
+const options = {dateStyle: "full", };
+    document.getElementById('current-date').textContent = new Date().toLocaleDateString('en-US', options);
