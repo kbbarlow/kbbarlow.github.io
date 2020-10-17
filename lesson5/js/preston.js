20c79c36fe7d8b7c1301827299b339fb5f6b2505
Brooke Barlow <brooke@barlowmstickers.lan>
2020-10-16 23:00:18 -0600

    .

--- lesson5/js/preston.js
+++ lesson5/js/preston.js
@@ -3,4 +3,24 @@ function toggleMenu(){
 }
 const options = {dateStyle: "full", };
     document.getElementById('current-date').textContent = new Date().toLocaleDateString('en-US', options);
-    
+
+function fridayBanner(){
+  let d = new Date();
+  const banner = document.querySelector('.prestonBanner');
+
+if (d.getDay() == 5){
+  banner.style.display = 'block';
+} else {
+  banner.style.display = 'none';
+}
+}  
+// function showBanner() {
+//   let d = new Date()
+//   const banner = document.getElementById('pancake-banner');
+
+//   if (d.getDay() == 5) {
+//       banner.style.display = "block";
+//   } else {
+//       banner.style.display = "none";
+//   }d.getDay()
+// }
