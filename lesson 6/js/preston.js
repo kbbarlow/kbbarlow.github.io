8d373e56840dbce40e86e3b62edfb8ce5cfb5944
kbbarlow <mrsbrookebarlow@gmail.com>
2020-10-21 12:06:03 -0600

    .

---
+++ lesson 6/js/preston.js
@@ -0,0 +1,18 @@
+function toggleMenu(){
+  document.getElementById("primaryNav").classList.toggle("hide");
+}
+const options = {dateStyle: "full", };
+    document.getElementById('current-date').textContent = new Date().toLocaleDateString('en-US', options);
+
+function fridayBanner(){
+  let d = new Date();
+  const banner = document.querySelector('.prestonBanner');
+
+if (d.getDay() == 5){
+  banner.style.display = 'block';
+} else {
+  banner.style.display = 'none';
+}
+console.log('today is ', d.getDay()); 
+}  
+window.addEventListener('DOMContentLoaded',fridayBanner);
