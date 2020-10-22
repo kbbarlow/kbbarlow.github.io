0656995808cd4177ae25a1818b011d77d947f95e
kbbarlow <mrsbrookebarlow@gmail.com>
2020-10-21 21:51:55 -0600

    .

--- lesson5/js/preston.js
+++ lesson5/js/preston.js
@@ -1,18 +1,19 @@
-function toggleMenu(){
+function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("hide");
 }
-const options = {dateStyle: "full", };
-    document.getElementById('current-date').textContent = new Date().toLocaleDateString('en-US', options);
+const options = {
+  dateStyle: "full",
+};
+document.getElementById('current-date').textContent = new Date().toLocaleDateString('en-US', options);
 
-function fridayBanner(){
+function fridayBanner() {
   let d = new Date();
   const banner = document.querySelector('.prestonBanner');
 
-if (d.getDay() == 5){
-  banner.style.display = 'block';
-} else {
-  banner.style.display = 'none';
+  if (d.getDay() == 5) {
+    banner.style.display = 'block';
+  } else {
+    banner.style.display = 'none';
+  }
 }
-console.log('today is ', d.getDay()); 
-}  
-window.addEventListener('DOMContentLoaded',fridayBanner);
+window.addEventListener('DOMContentLoaded', fridayBanner());
