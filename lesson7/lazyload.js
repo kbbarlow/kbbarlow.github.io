17589541ae904f80fba299da5baf9a905392d687
kbbarlow <mrsbrookebarlow@gmail.com>
2020-10-25 23:52:34 -0600

    .

---
+++ lesson7/lazyload.js
@@ -0,0 +1,29 @@
+const imges = document.querySelectorAll("[data-src]")
+function preloadImage(img) {
+  const src = img.getAttribute("data-src");
+  if(!src){
+    return;
+  }
+  img.src = src;
+}
+const imgOptions= {
+  threshold: 0;
+  rootMargin:"0px 0px 300px 0px"
+};
+
+const imgObserver = new IntersectionObserver((enteries, imgObserver) => {
+enteries.forEach(enter => {
+  if(!IntersectionObserverEntry.isIntersecting){
+    return;
+  }else{
+preloadImage(entry.target);
+imgObserver.unobserve(entry.target);
+  }
+
+  }
+})
+},imgOptions)
+
+images.forEach(image => {
+imgObserver.observe(image);
+})
