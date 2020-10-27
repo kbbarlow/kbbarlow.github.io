2a97153933ce29956fbc4b49859ed267bc61859a
kbbarlow <mrsbrookebarlow@gmail.com>
2020-10-27 16:43:22 -0600

    .

--- lesson5/js/preston.js
+++ lesson5/js/preston.js
@@ -10,7 +10,7 @@ window.onload = function fridayBanner() {
   let d = new Date();
   const ban = document.querySelector('.prestonBanner');
 
-  if (d.getDay() == 5) {
+  if (d.getDay() == 2) {
     ban.style.display = 'block';
   } else {
     ban.style.display = 'none';
