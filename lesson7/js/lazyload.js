e4c5a91e0540c0b439f9aad71bcb3274c633a28e
kbbarlow <mrsbrookebarlow@gmail.com>
2020-10-29 11:16:03 -0600

    .

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,8 +1,9 @@
 const imagesToLoad = document.querySelectorAll('img[data-src]'); 
 
 const imgOptions = {
-  threshold: 0,
-  rootMargin: "0 0 50px 0"
+  threshold: 0.5,
+  rootMargin: "0 0 50px 0",
+  
 };
 const loadImages =(image) =>{
   image.setAttribute('src', image.getAttribute('data-src'));
