5de21ebe867fd209d161922b50a1a2ebf5fdd5ea
kbbarlow <mrsbrookebarlow@gmail.com>
2020-10-31 00:51:37 -0600

    .

--- lesson6/js/preston.js
+++ lesson6/js/preston.js
@@ -1,32 +1,26 @@
-
-WebFont.load({ google: { families: ["Abril Fatface", "Poppins"] } });
 function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("hide");
 }
-const options = {
-  dateStyle: "full",
-};
-document.getElementById('current-date').textContent = new Date().toLocaleDateString('en-US', options);
-
-window.onload = function windChill() {
-  let t = parseFloat(document.getElementById('currentTemp').textContent);
-  let s= parseFloat(document.getElementById('currentWind').textContent); 
-  let output = "N/A";
-  if (t<= 50 && s>=3){
-  let f = (35.74 + (0.6215*t)) - (35.75 * (Math.pow(s, 0.16))) + (0.4275*(t*(Math.pow(s , 0.16))));
-  output = Math.round(f);
-  }
-  document.getElementById("output").innerHTML = output + " &#8457;";
-};
 
 window.onload = function fridayBanner() {
   let d = new Date();
   const ban = document.querySelector('.prestonBanner');
 
-  if (d.getDay() == 5) {
+  if (d.getDay() == 6) {
     ban.style.display = 'block';
   } else {
     ban.style.display = 'none';
   };
 };
 
+WebFont.load({
+  google: {
+    families: ["Abril Fatface", "Poppins"]
+  }
+});
+
+
+const options = {
+  dateStyle: "full",
+};
+document.getElementById('current-date').textContent = new Date().toLocaleDateString('en-US', options);
