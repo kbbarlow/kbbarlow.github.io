3e3f10b72e5af081b96bff8cc126f6d56e56b20d
kbbarlow <mrsbrookebarlow@gmail.com>
2020-11-18 14:42:56 -0700

    .

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -4,10 +4,9 @@ fetch(apiURL)
   .then((jsObject) => {
     console.log(jsObject);
     document.getElementById('current-temp').textContent = jsObject.main.temp;
-  
-const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  
-const desc = jsObject.weather[0].description;  
-document.getElementById('imagesrc').textContent = imagesrc; 
-document.getElementById('icon').setAttribute('src', imagesrc); 
-document.getElementById('icon').setAttribute('alt', desc);
-});
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+    const desc = jsObject.weather[0].description;
+    document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc);
+  });
