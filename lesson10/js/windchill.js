1987e476d098b2dba175ddd5323993f5aab5314b
kbbarlow <mrsbrookebarlow@gmail.com>
2020-11-18 16:12:57 -0700

    .

---
+++ lesson10/js/windchill.js
@@ -0,0 +1,10 @@
+(function windChill() {
+  let t = parseFloat(document.getElementById('currentTemp').textContent);
+  let s= parseFloat(document.getElementById('currentWind').textContent); 
+  let output = "N/A";
+  if (t<= 50 && s>=3){
+  let f = (35.74 + (0.6215*t)) - (35.75 * (Math.pow(s, 0.16))) + (0.4275*(t*(Math.pow(s , 0.16))));
+  output = Math.round(f);
+  }
+  document.getElementById("output").innerHTML = output + " &#8457;";
+}());
