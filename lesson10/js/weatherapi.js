edb39c8918f5ed58a992aa07d558c5b1c534cc78
kbbarlow <mrsbrookebarlow@gmail.com>
2020-11-18 13:46:31 -0700

    .

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "api.openweathermap.org/data/2.5/forecast?id=524901&appid={f59f532633b10d55bbf07be7f8538bff}";
+const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=f59f532633b10d55bbf07be7f8538bff';
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
