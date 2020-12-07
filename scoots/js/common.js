51b2b668fda69c9da08082ca57722f0ac0593776
kbbarlow <mrsbrookebarlow@gmail.com>
2020-12-06 22:22:09 -0700

    .

--- scoots/js/common.js
+++ scoots/js/common.js
@@ -1,3 +1,5 @@
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("hide");
 const options = {year: 'numeric'};
     document.getElementById('current-date').textContent = new Date().toLocaleDateString('en-US', options);
 document.getElementById('last-updated').innerHTML = document.lastModified;
