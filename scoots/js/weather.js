0880df1c46bdef1003aa96268c0d6e002f5c36f4
kbbarlow <mrsbrookebarlow@gmail.com>
2020-12-10 13:58:55 -0700

    .

--- scoots/js/weather.js
+++ scoots/js/weather.js
@@ -2,12 +2,16 @@ const APIurl ='https://api.openweathermap.org/data/2.5/onecall?lat=20.422984&lon
 fetch(APIurl)
 .then((response) => response.json())
 .then((jsObject) => {
-  console.table(jsObject);
+  console.log(jsObject);
+  const descipt = jsObject.current.weather[0].description;
+  const image ='https://openweathermap.org/img/wn/' + jsObject.current.weather[0].icon + '@2x.png';
+  document.getElementById('icon').setAttribute('alt', descipt);
+  document.getElementById('icon').setAttribute('src', image);
   document.getElementById('weather').innerHTML = jsObject.current.weather[0].description;
   document.getElementById('current-temp').innerHTML = Math.round(jsObject.current.temp) + "&#8457;";
   document.getElementById('feels_like').innerHTML = Math.round(jsObject.current.feels_like) + "&#8457;";
   document.getElementById('humidity').innerHTML = Math.round(jsObject.current.humidity) + "&#37;";
-  var forecast = jsObject.daily.slice(0, 3);
+  var forecast = jsObject.daily.slice(1, 4);
   const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   for (let day = 0; day < forecast.length; day++) {
     const d = new Date(forecast[day].dt * 1000); // The new Date() function needs a milliseconds value. Convert Unix timestamp to milliseconds by multiplying by 1000.
