fac9ce7b6eb60477c0017e9638647e3102681a0f
kbbarlow <mrsbrookebarlow@gmail.com>
2020-12-09 22:42:53 -0700

    .

--- scoots/js/weather.js
+++ scoots/js/weather.js
@@ -4,22 +4,18 @@ fetch(APIurl)
 .then((jsObject) => {
   console.table(jsObject);
   document.getElementById('weather').innerHTML = jsObject.current.weather[0].description;
-  document.getElementById('current-temp').innerHTML = jsObject.current.temp + "&#8457;";
-  document.getElementById('humidity').innerHTML = jsObject.current.humidity + "&#37;";
-  document.getElementById('feels_like').innerHTML = jsObject.current.feels_like + "&#8457;";
+  document.getElementById('current-temp').innerHTML = Math.round(jsObject.current.temp) + "&#8457;";
+  document.getElementById('feels_like').innerHTML = Math.round(jsObject.current.feels_like) + "&#8457;";
+  document.getElementById('humidity').innerHTML = Math.round(jsObject.current.humidity) + "&#37;";
+  var forecast = jsObject.list.filter(x => x.dt_txt.includes('max'));
+  const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  for (let day = 0; day < forecast.length; day++) {
+    const d = new Date(forecast[day].dt_txt);
+    const imagesrc = 'https://openweathermap.org/img/wn/' + forecast[day].daily.weather[2].icon;
+    const desc = forecast[day].daily.weather[0].description;
+    document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
+    document.getElementById(`forecast${day+1}`).textContent = Math.round(forecast[day].daily.temp[2]);
+    document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
+    document.getElementById(`icon${day+1}`).setAttribute('alt', desc);
+  }
 });
-// fetch(forecastURL)
-// .then((response) => response.json())
-// .then((forecastObject) => {
-//   var forecast = forecastObject.list.filter(x => x.dt_txt.includes('18:00:00'));
-//   const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-//   for (let day = 0; day < forecast.length; day++) {
-//     const d = new Date(forecast[day].dt_txt);
-//     const imagesrc = 'https://openweathermap.org/img/wn/' + forecast[day].weather[0].icon + '@2x.png';
-//     const desc = forecast[day].weather[0].description;
-//     document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
-//     document.getElementById(`forecast${day+1}`).textContent = Math.round(forecast[day].main.temp);
-//     document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
-//     document.getElementById(`icon${day+1}`).setAttribute('alt', desc);
-//   }
-// });
