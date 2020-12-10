b1a77355d36d7b3df0f8fc7a24940b10b27c6282
kbbarlow <mrsbrookebarlow@gmail.com>
2020-12-09 21:02:33 -0700

    .

--- scoots/js/weather.js
+++ scoots/js/weather.js
@@ -8,18 +8,18 @@ fetch(APIurl)
   document.getElementById('humidity').innerHTML = jsObject.main.humidity + "&#37;";
   document.getElementById('feels_like').innerHTML = Math.round(s) + " mph";
 });
-fetch(forecastURL)
-.then((response) => response.json())
-.then((forecastObject) => {
-  var forecast = forecastObject.list.filter(x => x.dt_txt.includes('18:00:00'));
-  const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  for (let day = 0; day < forecast.length; day++) {
-    const d = new Date(forecast[day].dt_txt);
-    const imagesrc = 'https://openweathermap.org/img/wn/' + forecast[day].weather[0].icon + '@2x.png';
-    const desc = forecast[day].weather[0].description;
-    document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
-    document.getElementById(`forecast${day+1}`).textContent = Math.round(forecast[day].main.temp);
-    document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
-    document.getElementById(`icon${day+1}`).setAttribute('alt', desc);
-  }
-});
+// fetch(forecastURL)
+// .then((response) => response.json())
+// .then((forecastObject) => {
+//   var forecast = forecastObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+//   const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+//   for (let day = 0; day < forecast.length; day++) {
+//     const d = new Date(forecast[day].dt_txt);
+//     const imagesrc = 'https://openweathermap.org/img/wn/' + forecast[day].weather[0].icon + '@2x.png';
+//     const desc = forecast[day].weather[0].description;
+//     document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
+//     document.getElementById(`forecast${day+1}`).textContent = Math.round(forecast[day].main.temp);
+//     document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
+//     document.getElementById(`icon${day+1}`).setAttribute('alt', desc);
+//   }
+// });
