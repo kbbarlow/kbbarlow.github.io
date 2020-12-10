ff7cc1bc3161d5d216c81e57d4098d47a16aa846
kbbarlow <mrsbrookebarlow@gmail.com>
2020-12-10 01:23:40 -0700

    .

--- scoots/js/weather.js
+++ scoots/js/weather.js
@@ -7,10 +7,10 @@ fetch(APIurl)
   document.getElementById('current-temp').innerHTML = Math.round(jsObject.current.temp) + "&#8457;";
   document.getElementById('feels_like').innerHTML = Math.round(jsObject.current.feels_like) + "&#8457;";
   document.getElementById('humidity').innerHTML = Math.round(jsObject.current.humidity) + "&#37;";
-  var forecast = jsObject.daily;
+  var forecast = jsObject.daily.slice(0, 3);
   const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   for (let day = 0; day < forecast.length; day++) {
-    const d = new Date(forecast[day].dt_txt);
+    const d = new Date(forecast[day].dt * 1000); // The new Date() function needs a milliseconds value. Convert Unix timestamp to milliseconds by multiplying by 1000.
     const imagesrc = 'https://openweathermap.org/img/wn/' + forecast[day].weather[0].icon + '@2x.png';
     const desc = forecast[day].weather[0].description;
     document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
