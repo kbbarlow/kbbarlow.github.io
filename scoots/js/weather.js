6bf5cea28391e67a5ef6fd6ce59e5713a06f036e
kbbarlow <mrsbrookebarlow@gmail.com>
2020-12-09 20:37:04 -0700

    .

--- scoots/js/weather.js
+++ scoots/js/weather.js
@@ -1,1 +1,2 @@
-https://api.openweathermap.org/data/2.5/onecall?lat=20.422984&lon=-86.922343&appid=f59f532633b10d55bbf07be7f8538bff;
+const APIurl ='https://api.openweathermap.org/data/2.5/onecall?lat=20.422984&lon=-86.922343&appid=f59f532633b10d55bbf07be7f8538bff;'
+console.log = APIurl;
