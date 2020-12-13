bebd9d107d2cfcbbc02375ead5aa80f4ef63ffd9
kbbarlow <mrsbrookebarlow@gmail.com>
2020-12-12 22:14:59 -0700

    .

---
+++ scoots/js/rental.js
@@ -0,0 +1,12 @@
+const request = 'data/pricing.json';
+fetch(request)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+    const desc = jsObject.weather[0].description;
+    document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc);
+  });
