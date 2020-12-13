157a572895f4a1e1184cda3c5034d06f66ade1fd
kbbarlow <mrsbrookebarlow@gmail.com>
2020-12-13 01:11:53 -0700

    .

--- scoots/js/rental.js
+++ scoots/js/rental.js
@@ -2,7 +2,6 @@ const request = 'https://kbbarlow.github.io/scoots/data/pricing.json';
 fetch(request)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
     const rentals = jsObject['rentals'];
     for (let i = 0; i < rentals.length; i++ ) {
       document.getElementById(`rhalfday${i+1}`).innerHTML = rentals[i].rhalfday;
